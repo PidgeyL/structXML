f4b52d63fa2fd41d23fde78ec9e5322221ae54b2
PidgeyL <pieterjan.moreels@gmail.com>
2015-04-17 09:31:35 +0200

    remove old code

--- structXML.py
+++ structXML.py
@@ -8,32 +8,14 @@
 
 # Imports
 import argparse
-from xml.sax import make_parser
-from xml.sax.handler import ContentHandler
 import xmltodict
 from collections import OrderedDict
 
 # Parse command line arguments
 argp=argparse.ArgumentParser(description='Analyze an XML file and return its structure')
-argp.add_argument('-i', help='xml file to analyze')
-argp.add_argument('-o', help='file to export structutre to')
+argp.add_argument('xml', metavar='xml', type=str, help='xml file to analyze')
+argp.add_argument('-o',  metavar='xml', type=str, help='file to export structutre to')
 args=argp.parse_args()
-
-# Parser
-class XMLHandler(ContentHandler):
-  def __init__(self):
-    self.tags={}
-  def startElement(self, name, attrs):
-    keys=list(self.tags.keys())
-    if not name in keys:
-      element={'attrs':attrs, 'recur':1}
-      self.tags[name]=element
-    else:
-      oldAttrs=self.tags[name]['attrs']
-      allAttrs=oldAttrs
-      # add new attrs
-      self.tags[name]['attrs']=allAttrs
-      self.tags[name]['recur']+=1
 
 def getAttr(dict):
   x=[]
@@ -72,19 +54,9 @@ def dig(dict,depth=0):
 
 
 if __name__ == '__main__':
-  if not args.i:
+  if not args.xml:
     sys.exit('No imputfile selected')
-  # create parser
-  parser=make_parser()
-  xh=XMLHandler()
-  parser.setContentHandler(xh)
-  parser.parse(args.i)
-  tags=xh.tags
-  #for x in tags:
-    #print("%s - recurs: %s times"%(x,tags[x]['recur']))
-  # xmltodict stuff
-  xml=open(args.i,'r').read()
+  xml=open(args.xml,'r').read()
   dict=xmltodict.parse(xml)
   struct=dig(dict)
-  #remove duplicates in list in dig
   print(struct)
